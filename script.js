80dc130bae04ace03c668c2e14e54c76a335ee67
JLee <thishereisjenn@gmail.com>
2021-11-19 20:51:39 -0800

    finished the bloody javascript and simple styling

--- script.js
+++ script.js
@@ -7,14 +7,22 @@ const questionsEl = document.querySelector("#questions");
 let questionEl = document.querySelector("#question");
 let questionCount = 0;
 
+const resultEl = document.querySelector("#result");
 const highscoresEl = document.querySelector("#highscores");
+let scoreListEl = document.querySelector("#score-list");
+let scoreList = [];
 const finalEl = document.querySelector("#final");
+let nameInput = document.querySelector("#name");
 const startBtn = document.querySelector("#start");
 const ans1Btn = document.querySelector("#answer1");
 const ans2Btn = document.querySelector("#answer2");
 const ans3Btn = document.querySelector("#answer3");
 const ans4Btn = document.querySelector("#answer4");
 const ansBtn = document.querySelectorAll("button.ansBtn")
+const submitScrBtn = document.querySelector("#submit-score");
+const goBackBtn = document.querySelector("#goback");
+const viewScrBtn = document.querySelector("#view-scores");
+const clearScrBtn = document.querySelector("#clearscores");
 
 const questions = [
     {
@@ -76,4 +84,101 @@ function setQuestion(id) {
         ans4Btn.textContent = questions[id].answers[3];
     }
 }
+
+function checkAnswer(event) {
+    event.preventDefault();
+
+    resultEl.style.display = "block";
+    let p = document.createElement("p");
+    resultEl.appendChild(p);
+
+    setTimeout(function () {
+        p.style.display = 'none';
+
+    }, 1000);
+
+    if (questions[questionCount].correctAnswer === event.target.value) {
+        p.textContent = "Correct!";
+    } else if (questions[questionCount].correctAnswer !== event.target.value) {
+        secondsLeft = secondsLeft - 15;
+        p.textContent = "Wrong!";
+    }
+
+    if (questionCount < questions.length) {
+        questionCount++;
+    }
+   
+    setQuestion(questionCount);
+}
+
+function addScore(event) {
+    event.preventDefault();
+
+    finalEl.style.display = "none";
+    highscoresEl.style.display = "block";
+
+    let init = nameInput.value.toUpperCase();
+    scoreList.push({ name: init, score: secondsLeft });
+
+    scoreList = scoreList.sort((a, b) => {
+        if (a.score < b.score) {
+          return 1;
+        } 
+        else {
+          return -1;
+        }
+      });
+    
+    scoreListEl.innerHTML="";
+    for (let i = 0; i < scoreList.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = `${scoreList[i].name}: ${scoreList[i].score}`;
+        scoreListEl.append(li);
+    }
+
+    
+    storeScores();
+    displayScores();
+}
+
+function storeScores() {
+    localStorage.setItem("scoreList",JSON.stringify(scoreList));
+}
+
+function displayScores() {
+    let storedScoreList = JSON.parse(localStorage.getItem("scoreList"));
+
+    if (storedScoreList !== null) {
+        scoreList = storedScoreList;
+    }
+}
+
+function clearScores() {
+    localStorage.clear();
+    scoreListEl.innerHTML="";
+}
+
 startBtn.addEventListener("click", startQuiz);
+
+ansBtn.forEach(item => {
+    item.addEventListener('click', checkAnswer);
+});
+
+submitScrBtn.addEventListener("click", addScore);
+
+goBackBtn.addEventListener("click", function () {
+    highscoresEl.style.display = "none";
+    firstEl.style.display = "block";
+    secondsLeft = 60;
+    timeEl.textContent = `Time:${secondsLeft}s`;
+});
+
+clearScrBtn.addEventListener("click", clearScores);
+
+viewScrBtn.addEventListener("click", function () {
+    if (highscoresEl.style.display === "none") {
+        highscoresEl.style.display = "block";
+    } else if (highscoresEl.style.display === "block") {
+        highscoresEl.style.display = "none";
+    } 
+});
